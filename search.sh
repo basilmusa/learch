a720a4b3473935192cfa76b0fd3c0a435e6933b2
Basil Abbas <babbas@isu.net.sa>
2011-11-29 11:31:52 +0300

    Saving all my work.

---
+++ search.sh
@@ -0,0 +1,1 @@
+java -cp target/search-1.0-SNAPSHOT.jar com.isu.App ${1}
---
+++ src/main/java/com/isu/App.java
@@ -0,0 +1,13 @@
+package com.isu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to learch, the linux search." );
+    }
+}
