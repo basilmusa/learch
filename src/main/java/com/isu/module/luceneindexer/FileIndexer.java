d4164e23ee4366ade072fff66684d83af36ef3b2
Basil Abbas <babbas@isu.net.sa>
2011-11-30 15:36:24 +0300

    Saving my work.

--- src/main/java/com/isu/module/luceneindexer/FileIndexer.java
+++ src/main/java/com/isu/module/luceneindexer/FileIndexer.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 
 import com.google.common.collect.ImmutableSet;
@@ -21,7 +22,7 @@ public class FileIndexer implements FileHandler
 {
 	private final long MAX_FILE_SIZE_TO_INDEX = (long) (0.5 * 1024 * 1024);
 	private final ImmutableSet<String> EXCLUDE_DIRECTORIES = ImmutableSet.of(".svn", ".git");
-	private final ImmutableSet<String> EXCLUDE_DIRECTORY_FULL_PATH = ImmutableSet.of("/proc");
+	private final ImmutableSet<String> EXCLUDE_DIRECTORY_FULL_PATH = ImmutableSet.of("/proc", "/sys", "/opt");
 	private final ImmutableSet<String> EXCLUDE_FILES = ImmutableSet.of();
 	
 	private IndexWriter indexWriter;
@@ -90,25 +91,51 @@ public class FileIndexer implements FileHandler
 		}
 
 			
-		// Now read the text inside it
-		indexFile(file);
+		// Now perform the indexing, if an error occurs, show an ugly
+		// stack trace, just print an ERROR and go to the next file
+		try 
+		{
+			indexFile(file);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 		
 		return Direction.NORMAL;
 	}
 	
 	private void indexFile(File file) {
 		System.out.println("Indexing document: [" + file.getAbsolutePath() + "]");
+		
+		FileReader fileReader;
+		
+		try {
+			fileReader = new FileReader(file); // To get the error as soon as possible
+		}
+		catch (IOException e) {
+			// Cant be read for some reason, just return
+			System.out.println("File cant be read for some reason.");
+			return;
+		}
+		
 		Document doc = new Document();
-		try 
-		{
-			doc.add(new Field("contents", new FileReader(file)));
+
+		try {
+			doc.add(new Field("contents", fileReader));
 			doc.add(new Field("filename", file.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED));
 			doc.add(new Field("fullpath", file.getCanonicalPath(), Field.Store.YES, Field.Index.NOT_ANALYZED));
 			indexWriter.addDocument(doc);
-		} catch (FileNotFoundException e) {
+		} catch (CorruptIndexException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		finally 
+		{
+			try {
+				fileReader.close();
+			} catch (IOException e) {}
 		}
 	}
 }
